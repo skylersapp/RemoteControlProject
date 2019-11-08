52b013e4d278c16f44d349ca9f17d5990f940d90
skylersapp <skylersapp@icloud.com>
2019-11-08 16:45:02 -0700

    Updated TODO comments

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -2,8 +2,9 @@
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
+	
 }
+	//TODO: Add pressButton() method
+	// TODO: Add turningON() method
