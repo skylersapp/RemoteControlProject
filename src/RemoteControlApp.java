0888dcefa6c8bc6f94929730314a4d7cf84cda90
skylersapp <skylersapp@icloud.com>
2019-11-08 18:02:22 -0700

    rough draft of project

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -4,90 +4,89 @@ public class RemoteControlApp {
 
 	public static void main(String[] args) {
 		Scanner key = new Scanner(System.in);
-		String on; 
-		String button; 
+		String on;
+		String button;
+
+		System.out.println("To begin, turn on the remote control.\n");
+		asciiRemote();
+		System.out.print(">>: ");
+		on = key.next().toLowerCase();
 		
-//		System.out.println("To begin, turn on the remote control.\n");
-//		asciiRemote();
-//		System.out.print(">>: ");
-//		on = key.next().toLowerCase();
-//		
-//		while (!on.equals("on")){
-//			System.out.println("\nTo begin, turn on the remote control.");
-//			System.out.println("\t\t(to turn on, type in a \"button press\"!)");
-//			System.out.print(">>: ");
-//			on = key.next();
-//		} 
-//		System.out.println();
-//		turningOn();
-//		turningOn();
-//		turningOn();
+		while (!on.equals("on")){
+			System.out.println("\nTo begin, turn on the remote control.");
+			System.out.println("\t\t(to turn on, type in a \"button press\"!)");
+			System.out.print(">>: ");
+			on = key.next();
+		} 
+		System.out.println();
+		turningOn();
+		turningOn();
+		turningOn();
 		System.out.println("\nPress any button for sound. \"OFF\" will turn remote off again. ");
 		System.out.print("Button press >>: ");
 		button = key.next().toLowerCase();
+
+		do {
+			switch (button) {
+
+				case "0":
+				case "1":
+				case "2":
+				case "3":
+				case "4":
+				case "5":
+				case "6":
+				case "7":
+				case "8":
+				case "9":
+					pressButton();
+					System.out.print(">>: ");
+					button = key.next().toLowerCase();
+					break;
+				case "off":
+					System.out.println();
+					turningOff();
+					turningOff();
+					turningOff();
+					break;
+				default:
+					System.out.println();
+					notRecognized();
+					System.out.print(">>: ");
+					button = key.next().toLowerCase();
+
+			}
+		} while (!button.equals("off"));
 		
-		switch (button) {
-			case "1":
-			case "2":
-			case "3":
-			case "4":
-			case "5":
-			case "6":
-			case "7":
-			case "8":
-			case "9":
-				pressButton();
-				break;
-			case "off":
-				System.out.println();
-				turningOff();
-				turningOff();
-				turningOff();
-				break;
-			default: 
-				notRecognized();
-				
-					
-					
-		
-		
-		
-		}
-		
-		
-		
-		
+		System.out.println();
+		turningOff();
+		turningOff();
+		turningOff();
 	}
+
 	public static void pressButton() {
 		System.out.println("BOOP!");
 	}
+
 	public static void turningOn() {
 		System.out.println("TURNING ON... ");
 	}
+
 	public static void turningOff() {
 		System.out.println("TURNING OFF...");
 	}
+
 	public static void notRecognized() {
 		System.out.println("COMMAND NOT RECOGNIZED.");
 	}
+
 	public static void asciiRemote() {
-		System.out.println(""
-				+ "\t _______________\n" + 
-				"\t/  ___________  \\\n" + 
-				"\t| | on || off | |\n" + 
-				"\t| |____||_____| |\n" + 
-				"\t|  ___ ___ ___  |\n" + 
-				"\t| | 7 | 8 | 9 | |\n" + 
-				"\t| |___|___|___| |\n" + 
-				"\t| | 4 | 5 | 6 | |\n" + 
-				"\t| |___|___|___| |\n" + 
-				"\t| | 1 | 2 | 3 | |\n" + 
-				"\t| |___|___|___| |\n" + 
-				"\t|     | 0 |     |\n" + 
-				"\t|     |___|     |\n" + 
-				"\t\\_______________/\n");
+		System.out.println("" + "\t _______________\n" + "\t/  ___________  \\\n" + "\t| | on || off | |\n"
+				+ "\t| |____||_____| |\n" + "\t|  ___ ___ ___  |\n" + "\t| | 7 | 8 | 9 | |\n" + "\t| |___|___|___| |\n"
+				+ "\t| | 4 | 5 | 6 | |\n" + "\t| |___|___|___| |\n" + "\t| | 1 | 2 | 3 | |\n" + "\t| |___|___|___| |\n"
+				+ "\t|     | 0 |     |\n" + "\t|     |___|     |\n" + "\t\\_______________/\n");
 	}
-	
+
 }
-	//TODO: Add pressButton() method
-	// TODO: Add turningON() method
+// TODO: Add pressButton() method
+// TODO: Add turningON() method
