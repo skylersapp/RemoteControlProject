6dae83cb1dca4eabaf95c1a46de748f16977f5be
skylersapp <skylersapp@icloud.com>
2019-11-08 17:16:58 -0700

    completed user stories one and two. halfway into user story 3.

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
 
 public class RemoteControlApp {
 
 	public static void main(String[] args) {
-
+		Scanner key = new Scanner(System.in);
+		String on; 
+		
+		System.out.println("\nTo begin, turn on the remote control.");
+		System.out.print(">>: ");
+		on = key.next().toLowerCase();
+		
+		while (!on.equals("on")){
+			System.out.println("\nTo begin, turn on the remote control.");
+			System.out.println("\t\t(to turn on, type in a \"button press\"!)");
+			System.out.print(">>: ");
+			on = key.next();
+		} 
+		System.out.println();
+		turningOn();
+		turningOn();
+		turningOn();
+		
+		
+		
+		
+	}
+	public static void pressButton() {
+		System.out.println("BOOP!");
+	}
+	public static void turningOn() {
+		System.out.println("TURNING ON... ");
 	}
 	
 }
