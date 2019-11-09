cb1763985832fcc0945d25b291cf27565ef9fccc
skylersapp <skylersapp@icloud.com>
2019-11-08 17:28:08 -0700

    added ascii remote

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -6,7 +6,8 @@ public class RemoteControlApp {
 		Scanner key = new Scanner(System.in);
 		String on; 
 		
-		System.out.println("\nTo begin, turn on the remote control.");
+		System.out.println("To begin, turn on the remote control.\n");
+		asciiRemote();
 		System.out.print(">>: ");
 		on = key.next().toLowerCase();
 		
@@ -30,6 +31,23 @@ public class RemoteControlApp {
 	}
 	public static void turningOn() {
 		System.out.println("TURNING ON... ");
+	}
+	public static void asciiRemote() {
+		System.out.println(""
+				+ "\t _______________\n" + 
+				"\t/  ___________  \\\n" + 
+				"\t| | on || off | |\n" + 
+				"\t| |____||_____| |\n" + 
+				"\t|  ___ ___ ___  |\n" + 
+				"\t| | 7 | 8 | 9 | |\n" + 
+				"\t| |___|___|___| |\n" + 
+				"\t| | 4 | 5 | 6 | |\n" + 
+				"\t| |___|___|___| |\n" + 
+				"\t| | 1 | 2 | 3 | |\n" + 
+				"\t| |___|___|___| |\n" + 
+				"\t|     | 0 |     |\n" + 
+				"\t|     |___|     |\n" + 
+				"\t\\_______________/\n");
 	}
 	
 }
